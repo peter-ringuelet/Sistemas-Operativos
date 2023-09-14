bf38ef0ea2885fc113e5ea9fd6d668f8496bf755
peter-ringuelet <peter.ringuelet@gmail.com>
2023-09-14 15:18:55 -0400

    hasta ej 7

--- Practica 3/practicashell-script/ej5/exito.sh
+++ Practica 3/practicashell-script/ej5/exito.sh
@@ -2,6 +2,8 @@
 # Supongamos que hay una condición que verifica si un archivo existe
 if [ -e "archivo.txt" ]; then
     echo "El archivo existe"
+    pwd
+    ls
     exit 0
 else
     echo "El archivo no existe"
