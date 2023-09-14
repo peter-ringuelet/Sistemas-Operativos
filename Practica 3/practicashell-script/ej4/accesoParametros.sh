0afc5d5cc3871626f61c91f406f411f2b772106e
peter-ringuelet <peter.ringuelet@gmail.com>
2023-09-14 13:03:45 -0400

    ej 5 terminado

---
+++ Practica 3/practicashell-script/ej4/accesoParametros.sh
@@ -0,0 +1,3 @@
+echo "El nombre del script es: $0"
+echo "El primer parámetro es: $1"
+echo "El segundo parámetro es: $2"
---
+++ Practica 3/practicashell-script/ej4/infoVariables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Total de parámetros: $#"
+echo "Todos los parámetros: $*"
+echo "Estado de salida del último comando: $?"
+echo "Directorio principal del usuario: $HOME"
