8fdb836b01de59ad6a1215b7aa625f28bea9eadb
peter-ringuelet <peter.ringuelet@gmail.com>
2023-09-19 11:16:00 -0400

    ej19 con p3,12,13

--- Practica 3/practicashell-script/ej19/19.sh
+++ Practica 3/practicashell-script/ej19/19.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-opciones="Ejercicio_3 Expresiones"
+opciones="Ejercicio_3 Expresiones Estructuras "
 select opt in $opciones; do
 	case $opt in
 		Ejercicio_3)
@@ -15,11 +15,47 @@ select opt in $opciones; do
 				case $optExpr in
 					A)
 						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej12/a.sh"
-						break;;
+						break
+					;;
+					B)
+						echo "Ingrese 2 numeros: "
+						read n1 n2
+						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej12/b.sh" $n1 $n2
+						break
+					;;
+					C)
+						echo "Ingrese una operacion y dos numeros: "
+						read op n1 n2
+						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej12/c.sh" $op $n1 $n2
+						break
+					;;
 				esac
 			done
-		break;;
+			break
+		;;
+		Estructuras)
+			echo "Estructuras de control"
+			opcionesEstr="A B C"
+			select optEstr in $opcionesEstr; do
+				case $optEstr in
+					A)
+						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej13/a.sh"
+						break
+					;;
+					B)
+						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej13/b.sh"
+						break
+					;;
+					C)
+						echo "Ingrese el nombre de un archivo: "
+						read nombre
+						"/home/peter/Desktop/Sistemas-Operativos/Practica 3/practicashell-script/ej13/c.sh" $nombre
+						break
+					;;
+				esac
+			done
+			break
+		;;
 	esac
-
 done
 
