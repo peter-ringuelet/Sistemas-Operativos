85b06f939762956b9b1fce053f9942b02c97d3d0
peter-ringuelet <peter.ringuelet@gmail.com>
2023-09-20 09:46:04 -0400

    un cambio

---
+++ Practica-3/practicashell-script/ej27/27.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+inicializar(){
+	array=()
+}
+
+agregar_elem(){
+	if [ $# -ne 1 ]; then
+		echo "Numero de parametros invalido"
+	else
+		array[${#array[@]}]=$1
+	fi
+}
+
+eliminar_elem(){
+	if [ $# -ne 1 ]; then
+		echo "Debe ingresar una posicion"
+	else
+		if [ $1 -lt ${#array[@]} ]; then
+			unset array[$1]
+			echo "Elemento en la posicion $1 eliminado con exito"
+		else
+			echo "La posicion esta fuera del rango del arreglo"
+		fi
+	fi
+}
+
+longitud(){
+	echo "${#array[@]}"
+}
+
+imprimir(){
+	echo "${array[@]}"
+}
+
+inicializar_con_valores(){
+	if [ $# -ne 2 ]; then
+		echo "Debe ingresar dos parametros, longitud y valor"
+	else
+		array=()
+		for (( i=0; i<$1; i++ )); do
+			array[$i]=$2
+		done
+	fi
+}
+
+inicializar_con_valores 10 personas
+agregar_elem Hola
+agregar_elem Chau
+imprimir
+eliminar_elem
+eliminar_elem 2
+eliminar_elem 1
+imprimir
+longitud
