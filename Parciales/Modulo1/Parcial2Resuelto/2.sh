f1c92ced3501b214925d1d6c85db2d728f3a9f6a
peter-ringuelet <peter.ringuelet@gmail.com>
2023-09-28 16:50:43 -0400

    Parcial 2

---
+++ Parciales/Modulo1/Parcial2Resuelto/2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo "Debe ingresar como minimo dos parametros"
+	exit 1
+else
+	directorio=$1
+	archivos=(${@:2})
+	if [ -e $directorio ]; then
+		echo "$directorio existe"
+		if [ -d $directorio ]; then
+			echo "$directorio es un directorio"
+			for archivo in ${archivos[@]}; do
+				if [ -e $directorio/$archivo ]; then
+					echo "$directorio/$archivo existe"
+					if [ -f $directorio/$archivo ]; then
+						echo "$directorio/$archivo es un archivo"
+						if [ -x $directorio/$archivo ]; then
+							echo "Se tien permiso de ejecucion sobre el archivo $directorio/$archivo"
+						fi
+					fi
+					if [ -d $directorio/$archivo ]; then
+						echo "$directorio/$archivo es un directorio"
+						if [ -w $directorio/$archivo ]; then
+							"Se tienen permisos de escritura sobre $directorio/$archivo"
+						fi
+					fi
+				fi
+			done
+		fi
+	fi
+fi
+
